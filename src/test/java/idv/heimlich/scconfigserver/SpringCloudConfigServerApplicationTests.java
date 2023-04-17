c770739ef07cc89d678a37d4d403a25ac6bbd686
Jerry L <jerry.l@acer.com>
2023-04-17 11:41:04 +0800

    first import

---
+++ src/test/java/idv/heimlich/scconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package idv.heimlich.scconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
